dde2b4bd8070df3e6359a9008cc4eff1f460d293
James Cool <jeffreysbrother@gmail.com>
2017-01-17 15:08:42 -0800

    not working

--- index.js
+++ index.js
@@ -3,8 +3,24 @@
 var fs = require('fs');
 var path = require('path');
 
-fs.readFile(path.resolve(__dirname, 'bears.txt'), function(err, data){
-  var bears = data.toString().split('\n');
-  var bear = bears[Math.floor(Math.random() * bears.length)];
-  console.log(bear);
+// fs.readFile(path.resolve(__dirname, 'bears.txt'), function(err, data){
+//   var bears = data.toString().split('\n');
+//   var bear = bears[Math.floor(Math.random() * bears.length)];
+//   console.log(bear);
+// });
+
+
+
+// fs.readdir(__dirname, function(err, files) {
+//     if (err) return;
+//     files.forEach(function(f) {
+//         console.log('Files: ' + f);
+//     });
+// });
+
+fs.readdir(__dirname, function(err, files) {
+    if (err) return;
+    files.forEach(function(f) {
+        f.replace(" ", "-").toLowerCase();
+    });
 });
