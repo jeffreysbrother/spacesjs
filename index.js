b853969d0d1a6a38d87bcd83527afcb9787e49ae
James Cool <jeffreysbrother@gmail.com>
2017-01-18 19:27:19 -0800

    eliminate other arrow function LOL

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ const target = './';
 const fs = require('fs');
 
 fs.readdir(target, function(err, files) {
-  files.forEach(file => {
+  files.forEach(function(file) {
     fs.rename(file, file.replace(/(\s+)|(-{2,})|(\t)/g, "-").toLowerCase(), function(err) {
     if (err) throw err;
-      // is something supposed to go here?
+      // is something supposed to go here, LOL?
     });
   });
   console.log('files renamed!');
