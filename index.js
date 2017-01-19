f9399333197ecde0e87cdfa8b894e354e39dca3b
James Cool <jeffreysbrother@gmail.com>
2017-01-19 09:26:53 -0800

    tims suggestions for RegEx

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 fs.readdir(target, function(err, files) {
   files.forEach(function(file) {
-    fs.rename(file, file.replace(/(-\s)|(\s-)|(\s+)|(-{2,})|(\t)/g, "-").toLowerCase(), function(err) {
+    fs.rename(file, file.replace(/([\s\-]{1,})/g, "-").toLowerCase(), function(err) {
     if (err) throw err;
       // is something supposed to go here, LOL?
     });
