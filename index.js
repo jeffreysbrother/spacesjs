d71f7edef134b2e2dc3149f2a6a4c7f580dcf347
James Cool <jeffreysbrother@gmail.com>
2017-01-23 12:54:15 -0800

    add curly braces to err and change console.log location

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var fs = require('fs');
 var argv = require('yargs').argv;
-var target = './';
+var target = process.cwd();
 var re = /([\s\-]{1,})/g;
 var flag = argv.remove;
 
@@ -10,23 +10,27 @@ if (flag) {
   fs.readdir(target, function(err, files) {
     files.forEach(function(file) {
       fs.rename(file, file.replace(argv.remove, ""), function(err) {
-        if (err)
+        if (err) {
           throw err;
+        }
       });
       fs.rename(file, file.replace(re, "-").toLowerCase(), function(err) {
-        if (err)
+        if (err) {
           throw err;
+        }
       });
     });
+    console.log('Files renamed! Additional string \"' + argv.remove + '\" removed.');
   });
 } else {
   fs.readdir(target, function(err, files) {
     files.forEach(function(file) {
       fs.rename(file, file.replace(re, "-").toLowerCase(), function(err) {
-        if (err)
+        if (err) {
           throw err;
+        }
       });
     });
   });
+  console.log('Files renamed!');
 }
-console.log('Files renamed!');
